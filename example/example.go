3667e259b0ac419e0bdb13715ab79eb16553bbdf
brucewang <289022650@qq.com>
2024-08-30 20:02:28 +0800

    1

--- example/example.go
+++ example/example.go
@@ -1,9 +1,19 @@
 package example
 
+import "fmt"
+
 func Add(a, b int) int {
 	return a + b
 }
 
 func Sub(a, b int) int {
 	return a - b
+}
+
+func Multi(a, b int) int {
+	return a * b
+}
+
+func init() {
+	fmt.Println("测试包已初始化")
 }
